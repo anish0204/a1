8d3c1d69c2fedf69d5e0263839decc9c16480988
anish0204 <47878071+anish0204@users.noreply.github.com>
2019-02-22 12:50:15 +0530

    Update a1.c

--- a1.c
+++ a1.c
@@ -4,7 +4,7 @@ int *fetch();
 int main()
 {
     int a[10] = {1,2,3,4,5,6,7,8,9,10};   //1
-    printf("%d",a[10]);
+    printf("%d",a[9]);
 
     int x,y;                              //2
     int *ptr;                             

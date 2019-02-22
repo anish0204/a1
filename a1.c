b469769c43e80c76cbe9416fe4f719e9b0d509df
anish0204 <47878071+anish0204@users.noreply.github.com>
2019-02-22 11:22:13 +0530

    Create a1.c

---
+++ a1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[10] = {1,2,3,4,5,6,7,8,9,10};
+    printf(a[11]);
+    char a[5] = {'a','b','s','v','l'};
+
+    int x,y;
+    int *ptr;
+    ptr = fetch();
+    int i = 1;
+    i = i++ * ++i;
+    printf(i);
+    printf("val = %d",*ptr);
+    return 0;
+    void *p1;
+    printf(sizeof(p1));
+    int b;
+    b++;
+
+}
+
+int *fetch()
+{
+    int val = 100;
+    return &val;
+}

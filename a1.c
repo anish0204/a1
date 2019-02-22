4f25e8640e7559476fbb11e9895714ad99c0ae0c
anish0204 <47878071+anish0204@users.noreply.github.com>
2019-02-22 12:45:06 +0530

    Update a1.c

--- a1.c
+++ a1.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+int *fetch();
 int main()
 {
-    int a[10] = {1,2,3,4,5,6,7,8,9,10};
-    printf("%d",a[9]);
-    //char a[5] = {'a','b','s','v','l'};
-    
-    int x,y;
-    int *ptr;
-    int a = 10;
+    int a[10] = {1,2,3,4,5,6,7,8,9,10};   //1
+    printf(a[11]);
+
+    int x,y;                              //2
+    int *ptr;                             
+    ptr = fetch();
     int i = 1;
-    i = i++ * ++i;
-    printf(i);
-    ptr = &a;
+    printf("%d",i);
     printf("val = %d",*ptr);
     return 0;
-    int *p1;
+    void *p1;
     printf(sizeof(p1));
-    int b =1;
+    int b;                              //4
     b++;
-    printf("%d",b);
+
 }
 
-
+int *fetch()
+{
+    int val = 100;
+    return &val;
+}                                   //3

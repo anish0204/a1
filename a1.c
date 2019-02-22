f3fc1a7d4607cd21bc8336c5303e9f29c9005a6e
anish0204 <47878071+anish0204@users.noreply.github.com>
2019-02-22 12:48:24 +0530

    Update a1.c

--- a1.c
+++ a1.c
@@ -4,7 +4,7 @@ int *fetch();
 int main()
 {
     int a[10] = {1,2,3,4,5,6,7,8,9,10};   //1
-    printf("%d",a[11]);
+    printf("%d",a[10]);
 
     int x,y;                              //2
     int *ptr;                             
@@ -13,15 +13,15 @@ int main()
     printf("%d",i);
     printf("val = %d",*ptr);
     return 0;
-    void *p1;
+    int *p1;
     printf(sizeof(p1));
-    int b;                              //4
+    int b=1;                              //4
     b++;
 
 }
 
-int *fetch()
-{
-    int val = 100;
-    return &val;
-}                                   //3
+//int *fetch()
+//{
+  //  int val = 100;
+    //return &val;
+//}                                   //3
